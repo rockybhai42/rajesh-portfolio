6039e19b8b366370ad24119fa03c5d105809cac1
Rajesh Kumar <rajeshskkanagaraj2001@gmail.com>
2026-06-02 11:06:48 +0530

    testing

--- client/my-portfolio-app/src/pages/Home.jsx
+++ client/my-portfolio-app/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import "./styles/app.css";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 import { useState,useEffect } from "react";
 
 function Home (){
