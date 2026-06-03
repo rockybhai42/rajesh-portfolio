70e452cb38d775a19d5fe202e21bc84df173765f
Rajesh Kumar <rajeshskkanagaraj2001@gmail.com>
2026-06-03 08:56:35 +0530

    ssl test

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const app = express();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+   ssl: {
+     rejectUnauthorized: false
+   }
 });
 
 /* =========================
